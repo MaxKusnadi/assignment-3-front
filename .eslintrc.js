5453c2275255c499923b32721e6802a14fb8ca08
Goh Wei Wen <goweiwen@gmail.com>
2017-09-11 17:02:01 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: [
+    'standard',
+    'prettier',
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'html',
+    'prettier'
+  ],
+  // add your custom rules here
+  'rules': {
+    'prettier/prettier': ['error', {
+      'semi': false,
+      'singleQuote': true,
+      'trailingComma': 'es5',
+      'bracketSpacing': true,
+    }],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
