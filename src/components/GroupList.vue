37d36423c41e10eb4a6423ad692ee80a77f75f44
Goh Wei Wen <goweiwen@gmail.com>
2017-09-14 00:39:43 +0800

    Fix page transition animations

--- src/components/GroupList.vue
+++ src/components/GroupList.vue
@@ -2,19 +2,20 @@
   <v-list dense>
     <v-subheader class="mt-3 grey--text text--darken-1">GROUPS</v-subheader>
     <v-list>
-      <v-list-tile v-for="group in Object.values(groups)" :key="group.id" avatar @click="$router.push(`/g/${group.id}`)">
+      <v-list-tile v-for="group in Object.values(groups)" :key="group.id" avatar @click="$router.push(`/g/${group.id}/`)">
         <v-list-tile-avatar>
           <img :src="`https://source.unsplash.com/${group.id}/256x256`" alt="">
         </v-list-tile-avatar>
         <v-list-tile-title v-text="group.name"></v-list-tile-title>
       </v-list-tile>
+      <v-list-tile avatar @click="$router.push('/createGroup/')">
+        <v-list-tile-avatar>
+          <v-icon class="grey--text text--darken-1">add</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-title class="grey--text text--darken-1">Create a Group</v-list-tile-title>
+      </v-list-tile>
     </v-list>
-    <v-list-tile class="mt-3" @click="$router.push('/createGroup')">
-      <v-list-tile-action>
-        <v-icon class="grey--text text--darken-1">add_circle_outline</v-icon>
-      </v-list-tile-action>
-      <v-list-tile-title class="grey--text text--darken-1">Create a Group</v-list-tile-title>
-    </v-list-tile>
+    <v-divider></v-divider>
     <v-list-tile @click="">
       <v-list-tile-action>
         <v-icon class="grey--text text--darken-1">settings</v-icon>
