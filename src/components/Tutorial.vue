c6e713eb842e2613c3f1a1e3d286508908a5d723
Goh Wei Wen <goweiwen@gmail.com>
2017-09-29 14:34:27 +0800

    Change Tutorial to use Stepper

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -1,24 +1,80 @@
 <template>
-  <div>
-    <v-carousel v-bind:cycle="false" class="car">
-      <v-carousel-item>
-        <img src="../assets/tutorial-01.png" />
-      </v-carousel-item>
-      <v-carousel-item>
-        <img src="../assets/tutorial-02.png" />
-      </v-carousel-item>
-     <v-carousel-item>
-        <img src="../assets/tutorial-03.png" />
-      </v-carousel-item>
-      <v-carousel-item>
-        <img src="../assets/tutorial-04.png" />
-      </v-carousel-item>
-      <v-carousel-item class="last">
-        <img src="../assets/tutorial-05.png" />
-        <v-btn primary dark large class="button" @click="finish">Start</v-btn>
-      </v-carousel-item>
-    </v-carousel>
-  </div>
+  <v-app>
+    <div class="tutorial">
+      <v-stepper v-model="tutorialState" non-linear>
+        <v-stepper-header>
+          <v-stepper-step step="1" :complete="tutorialState > 1">Create Groups</v-stepper-step>
+          <v-divider></v-divider>
+          <v-stepper-step step="2" :complete="tutorialState > 2">Create Events</v-stepper-step>
+          <v-divider></v-divider>
+          <v-stepper-step step="3" :complete="tutorialState > 3">Take Attendance</v-stepper-step>
+          <v-divider></v-divider>
+          <v-stepper-step step="4" :complete="tutorialState > 4">RSVP</v-stepper-step>
+          <v-divider></v-divider>
+          <v-stepper-step step="5">Attend!</v-stepper-step>
+        </v-stepper-header>
+        <div v-show="tutorialState === 1">
+          <v-card>
+            <v-card-media>
+              <img src="../assets/tutorial-01.png" />
+            </v-card-media>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat @click.native="finish">Skip</v-btn>
+              <v-btn primary @click.native="tutorialState = 2">Got It</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+        <div v-show="tutorialState === 2">
+          <v-card>
+            <v-card-media>
+              <img src="../assets/tutorial-02.png" />
+            </v-card-media>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat @click.native="finish">Skip</v-btn>
+              <v-btn primary @click.native="tutorialState = 3">Got It</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+        <div v-show="tutorialState === 3">
+          <v-card>
+            <v-card-media>
+              <img src="../assets/tutorial-03.png" />
+            </v-card-media>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat @click.native="finish">Skip</v-btn>
+              <v-btn primary @click.native="tutorialState = 4">Got It</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+        <div v-show="tutorialState === 4">
+          <v-card>
+            <v-card-media>
+              <img src="../assets/tutorial-04.png" />
+            </v-card-media>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat @click.native="finish">Skip</v-btn>
+              <v-btn primary @click.native="tutorialState = 5">Got It</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+        <div v-show="tutorialState === 5">
+          <v-card>
+            <v-card-media>
+              <img src="../assets/tutorial-05.png" />
+            </v-card-media>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn primary @click.native="finish">That's all!</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-stepper>
+    </div>
+  </v-app>
 </template>
 
 <script>
@@ -26,9 +82,13 @@ export default {
   mounted() {
     this.$store.dispatch('fetchGroups')
   },
+
   data() {
-    return {}
+    return {
+      tutorialState: 1,
+    }
   },
+
   methods: {
     finish() {
       console.log('okim here')
@@ -39,20 +99,23 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.car
-  height: -webkit-fill-available
-  z-index: 1
+.tutorial
+  background-color: #fff
 
-img
-  width: 100%
-  
-.last
-  align-items: center
-  width: 100%
 .button
   position: fixed
   z-index: 2
   bottom: 60px
 
+img
+  height: fill-available
+  object-fit: contain
+
+.card__actions
+  background-color: #fff
+  position: fixed
+  bottom: 0
+  left: 0
+  right: 0
 
 </style>
