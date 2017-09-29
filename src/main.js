96b09b14a950dc5db376b327311a2553a67f2226
Goh Wei Wen <goweiwen@gmail.com>
2017-09-29 22:14:20 +0800

    Defer Facebook

--- src/main.js
+++ src/main.js
@@ -70,13 +70,3 @@ window.fbAsyncInit = function() {
 
   FB.AppEvents.logPageView()
 }
-
-/* eslint-disable */
-/* prettier-ignore */
-;(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
