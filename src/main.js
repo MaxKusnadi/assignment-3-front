ea317fec5554e321db44356b06135539781a077c
Goh Wei Wen <goweiwen@gmail.com>
2017-09-29 23:45:36 +0800

    Move Facebook into script

--- src/main.js
+++ src/main.js
@@ -52,6 +52,15 @@ new Vue({
 })
 
 // Load Facebook SDK
+// prettier-ignore
+/* eslint-disable */
+;(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 window.fbAsyncInit = function() {
   FB.init({
     appId: '1935737966751741',
