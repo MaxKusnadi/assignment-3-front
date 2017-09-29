02e0a7f64da3d5e88d756d12e5958c3737cbf506
Goh Wei Wen <goweiwen@gmail.com>
2017-09-30 00:10:08 +0800

    Remove night mode setting

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,17 +1,12 @@
 <template>
   <v-list two-line subheader>
     <v-subheader>General</v-subheader>
-    <v-list-tile avatar>
+    <v-list-tile avatar tag="div" :ripple="false">
       <v-list-tile-content>
         <v-list-tile-title>Notification Alerts</v-list-tile-title>
         <v-list-tile-sub-title>Enable notification alerts for upcoming events</v-list-tile-sub-title>
       </v-list-tile-content>
       <v-list-tile-action><v-switch v-model="settings.notifications" @click="toggleNotifications"></v-switch></v-list-tile-action>
-      <v-list-tile-content>
-        <v-list-tile-title>Night Mode</v-list-tile-title>
-        <v-list-tile-sub-title>Automatically enable night mode at sundown</v-list-tile-sub-title>
-      </v-list-tile-content>
-      <v-list-tile-action><v-switch v-model="settings.nightMode" @click="toggleNightMode"></v-switch></v-list-tile-action>
     </v-list-tile>
   </v-list>
 </template>
--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -1,6 +1,5 @@
 const state = {
   notifications: true,
-  nightMode: false,
   primary: 'teal',
   accent: 'pink accent-2',
 }
@@ -12,17 +11,11 @@ const actions = {
     // api('post', 'notifications')
     commit('setNotifications', { notifications })
   },
-  setNightMode({ commit }, { nightMode }) {
-    commit('setNightMode', { nightMode })
-  },
 }
 
 const mutations = {
   setNotifications(state, { notifications }) {
     state.notifications = notifications
-  },
-  setNightMode(state, { nightMode }) {
-    state.nightMode = nightMode
   },
 }
 
