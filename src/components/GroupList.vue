27401e43f006152f402447c9bfd781525f754964
BaiChuann <buerlanbo2@gmail.com>
2017-10-03 02:52:57 +0800

    onboard add group button

--- src/components/GroupList.vue
+++ src/components/GroupList.vue
@@ -1,30 +1,35 @@
 <template>
   <v-container fluid grid-list-md >
-    <v-layout row wrap>
-      <v-flex
-        xs12 md6
-        v-for="group in Object.values(groups)"
-        :key="group.groupId"
-        @click="$router.push(`/g/${group.groupId}/`)"
-      >
-        <v-card
-          class="group"
-          hover
+    <div v-if="noGroup" class="noGroup">
+      <v-btn primary dark large class="createGroup" @click="$router.push(`/createGroup/`)">Create your first Group</v-btn> 
+    </div>
+    <div v-else>
+      <v-layout row wrap>
+        <v-flex
+          xs12 md6
+          v-for="group in Object.values(groups)"
+          :key="group.groupId"
+          @click="$router.push(`/g/${group.groupId}/`)"
         >
-          <v-card-media
-            :src="group.pic_url || ''" alt=""
-            height="300px"
+          <v-card
+            class="group"
+            hover
           >
-          </v-card-media>
-          <v-card-title primary-title>
-            <h3 class="headline mb-0" v-text="group.name"></h3>
-          </v-card-title>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-btn dark :class="accent" fixed fab bottom right @click="$router.push(`/createGroup/`)">
-      <icon name="plus"></icon>
-    </v-btn>
+            <v-card-media
+              :src="group.pic_url || ''" alt=""
+              height="300px"
+            >
+            </v-card-media>
+            <v-card-title primary-title>
+              <h3 class="headline mb-0" v-text="group.name"></h3>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      <v-btn dark :class="accent" fixed fab bottom right @click="$router.push(`/createGroup/`)">
+        <icon name="plus"></icon>
+      </v-btn>
+    </div>
   </v-container>
 </template>
 
@@ -41,6 +46,9 @@ export default {
       groups: state => state.groups,
       accent: state => state.settings.accent,
     }),
+    noGroup: function() {
+      return Object.keys(this.groups).length === 0
+    }
   },
 }
 </script>
@@ -55,4 +63,21 @@ export default {
 
 .button-wrapper
   position: relative
+
+.noGroup
+  background-image url(/static/img/bg.webp)
+  background-repeat repeat
+  position fixed
+  left 0
+  right 0
+  z-index 2
+  min-height 100vh
+
+.createGroup
+  background-color: grey
+  position: fixed
+  top: 200px
+  width: 70%
+  margin-left: 15%
+  margin-right: 15%
 </style>
